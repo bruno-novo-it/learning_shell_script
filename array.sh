cce357e9a0d24666c4a3483413fe3d1aa2660a10
Bruno Novo <brunonovo@MacBook-Pro-de-Bruno.local>
2017-02-22 08:04:55 -0300

    Arquivo de array.sh e popula_array.txt

---
+++ array.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+
+#################################
+# Populando e utilizando Arrays #
+#################################
+
+array=()
+
+function popula_array(){
+while read LINE
+do
+  if [ "$(($LINE % 2))" -eq 0 ]; then
+    array=("${array[@]}" "$LINE")
+  fi
+done < popula_array.txt
+}
+
+function print_array(){
+  echo -e "\n######### Array #########\n"
+  for i in ${array[@]}
+  do
+    echo -e "Valor: $i"
+  done
+  # Mostra o Total de elementos
+  echo -e "\nTotal de Elementos: ${#array[@]}"
+}
+
+popula_array
+
+print_array
