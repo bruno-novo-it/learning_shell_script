cfe6eb6867c13f94b175244db7da2af966faca4b
Lucas Mucheroni <lucas.mucheroni@concrete.com.br>
2017-02-23 17:27:53 -0300

    Criando Menu para calculadora

--- funcoes.sh
+++ funcoes.sh
@@ -2,38 +2,54 @@
 
 ###################################################################
 # Base de funções para cálculos numéricos utilizando shell script #
-# Feito por Bruno Luís e Elias Costa                              #
+# Feito por Bruno Luís, Elias Costa, Lucas Mucheroni              #
 ###################################################################
 
 
 function soma(){
+  echo -e "\n\n#############################"
+  echo "##           SOMA          ##"
+  echo -e "#############################\n"
   echo "Digite o primeiro valor: "
   read first
   echo "Digite o segundo Valor: "
   read second
   echo "A soma dos dois valores é: $((first + second))"
+  Menu
 }
 
 function subtracao(){
+  echo -e "\n\n#############################"
+  echo "##        SUBTRAÇÃO        ##"
+  echo -e "#############################\n"
   echo "Digite o primeiro valor: "
   read first
   echo "Digite o segundo Valor: "
   read second
   echo "A subtração dos dois valores é: $((first - second))"
+  Menu
 }
 
 function multiplicacao(){
+  echo -e "\n\n#############################"
+  echo "##      MULTIPLICAÇÃO      ##"
+  echo -e "#############################\n"
   echo "Digite o primeiro valor: "
   read first
   echo "Digite o segundo Valor: "
   read second
   echo "A multiplicação dos dois valores é: $((first * second))"
+  Menu
 }
 
 function divisao(){
+  echo -e "\n\n#############################"
+  echo "##         DIVISÃO         ##"
+  echo -e "#############################\n"
   echo "Digite o primeiro valor: "
   read first
   echo "Digite o segundo Valor: "
   read second
   echo "A divisão dos dois valores é: $((first / second))"
+  Menu
 }
